9c4106435d151128151b248c79aefa1987c93d8f
ToufiqAli <mohammadtoufiqali90@gmail.com>
2026-07-10 23:28:15 +0530

    Deployed on Vercel everything

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const cors = require('cors');
 const ConnectDB = require('./config/db');
+const dotenv = require('dotenv');
 
 
 const userRoutes = require('./Routes/user.route');
 const categoryRoutes = require ('./Routes/category.route');
 const productRoutes = require('./Routes/products.route');
 
+dotenv.config();
 
 ConnectDB();
 
@@ -17,9 +19,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use((req, res) => {
-  res.status(200).json({message:"Hello From Server"});
-});
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message : "This Is UR Server Running .....",
+        PORT : `The Server is Running on ${process.env.PORT}`
+    })
+})
 app.use('/users', userRoutes);
 app.use('/category', categoryRoutes)
 app.use('/products',productRoutes);
