0dccb832fa2fa408a8c55f6923854e73b27e6b0b
ToufiqAli <mohammadtoufiqali90@gmail.com>
2026-07-13 23:44:20 +0530

    crete order added

--- src/Controllers/orders.controller.js
+++ src/Controllers/orders.controller.js
@@ -1,1 +1,31 @@
-const customer = require('../')
+const orderservice = require ('../Services/order.service');
+const customer = require('../model/user')
+
+
+
+class OrderController{
+    async orderCreate(req,res,next){
+        try{
+        const orderdata = req.body;
+        const validcustomer = await customer.find({customer:orderdata.customer});
+        if(!validcustomer){
+            res.status(404).json({
+                status : "Failed",
+                message : "Not An Valid User"
+            })
+        }
+        else{
+            const order = orderservice.createorder(orderdata);
+            res.status(200).json({
+                status : "Success",
+                order
+            })
+
+        }
+    }catch(error){
+        next(error);
+    }
+
+
+    }
+}
---
+++ src/Services/order.service.js
@@ -0,0 +1,12 @@
+const orderScheme = require('../model/orders');
+const crypto = require('crypto');
+
+
+class orderServices{
+    async createorder(order){
+        const orderId = `ORD_${crypto.randomBytes(4).toString('hex')}`
+        order.orderId = orderId;
+        const createorder = await orderScheme.create(order);
+        return createorder
+    }
+}
